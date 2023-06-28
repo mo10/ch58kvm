f2eeede8b1db6c2a5ad65c878384ef59e11aee05
mo10 <0w0@0w0.pw>
2023-06-29 03:13:30 +0800

    Bind hid to usb2 and blink LED.

--- src/main.c
+++ src/main.c
@@ -1,108 +1,48 @@
-/********************************** (C) COPYRIGHT *******************************
- * File Name          : Main.c
- * Author             : WCH
- * Version            : V1.0
- * Date               : 2020/08/06
- * Description        : 串口1收发演示
- *********************************************************************************
- * Copyright (c) 2021 Nanjing Qinheng Microelectronics Co., Ltd.
- * Attention: This software (modified or not) and binary are used for
- * microcontroller manufactured by Nanjing Qinheng Microelectronics.
- *******************************************************************************/
+#include "main.h"
 
 #include "CH58x_common.h"
+#include "hid.h"
 
-uint8_t TxBuff[] = "This is a tx exam\r\n";
-uint8_t RxBuff[100];
-uint8_t trigB;
+static void debug_uart_init() {
+    GPIOA_SetBits(GPIO_Pin_9);
+    GPIOA_ModeCfg(GPIO_Pin_8, GPIO_ModeIN_PU);
+    GPIOA_ModeCfg(GPIO_Pin_9, GPIO_ModeOut_PP_5mA);
+    UART1_DefInit();
+}
 
-/*********************************************************************
- * @fn      main
- *
- * @brief   主函数
- *
- * @return  none
- */
-int main()
-{
-    uint8_t len;
-
+int main() {
     SetSysClock(CLK_SOURCE_PLL_60MHz);
 
-    /* 配置串口1：先配置IO口模式，再配置串口 */
-    GPIOA_SetBits(GPIO_Pin_9);
-    GPIOA_ModeCfg(GPIO_Pin_8, GPIO_ModeIN_PU);      // RXD-配置上拉输入
-    GPIOA_ModeCfg(GPIO_Pin_9, GPIO_ModeOut_PP_5mA); // TXD-配置推挽输出，注意先让IO口输出高电平
-    UART1_DefInit();
-
-#if 1 // 测试串口发送字符串
-    UART1_SendString(TxBuff, sizeof(TxBuff));
-
+#if defined(DEBUG)
+    debug_uart_init(); /* Enable debug uart */
 #endif
 
-#if 1 // 查询方式：接收数据后发送出去
-    while(1)
-    {
-        len = UART1_RecvString(RxBuff);
-        if(len)
-        {
-            UART1_SendString(RxBuff, len);
-        }
-    }
+    /* Status LED init */
+    GPIOB_ResetBits(STATUS_LED_PIN);
+    GPIOB_ModeCfg(STATUS_LED_PIN, GPIO_ModeOut_PP_5mA);
+    TMR0_TimerInit(FREQ_SYS / 1); // 设置定时时间 100ms
+    TMR0_ITCfg(ENABLE, TMR0_3_IT_CYC_END); // 开启中断
+    PFIC_EnableIRQ(TMR0_IRQn);
 
-#endif
+    // USB1 init
+//    hid_init(0);
+//    PFIC_EnableIRQ(USB_IRQn);
 
-#if 0 // 中断方式：接收数据后发送出去
-    UART1_ByteTrigCfg(UART_7BYTE_TRIG);
-    trigB = 7;
-    UART1_INTCfg(ENABLE, RB_IER_RECV_RDY | RB_IER_LINE_STAT);
-    PFIC_EnableIRQ(UART1_IRQn);
-#endif
+    // USB2 init
+
+    printf("\r\nUSB2 Init\r\n");
+    hid_init(1);
+    PFIC_EnableIRQ(USB2_IRQn);
 
     while(1);
 }
 
-/*********************************************************************
- * @fn      UART1_IRQHandler
- *
- * @brief   UART1中断函数
- *
- * @return  none
- */
-__INTERRUPT
-        __HIGH_CODE
-void UART1_IRQHandler(void)
-{
-    volatile uint8_t i;
-
-    switch(UART1_GetITFlag())
-    {
-        case UART_II_LINE_STAT: // 线路状态错误
-        {
-            UART1_GetLinSTA();
-            break;
-        }
-
-        case UART_II_RECV_RDY: // 数据达到设置触发点
-            for(i = 0; i != trigB; i++)
-            {
-                RxBuff[i] = UART1_RecvByte();
-                UART1_SendByte(RxBuff[i]);
-            }
-            break;
-
-        case UART_II_RECV_TOUT: // 接收超时，暂时一帧数据接收完成
-            i = UART1_RecvString(RxBuff);
-            UART1_SendString(RxBuff, i);
-            break;
-
-        case UART_II_THR_EMPTY: // 发送缓存区空，可继续发送
-            break;
-
-        case UART_II_MODEM_CHG: // 只支持串口0
-            break;
-
-        default:
-            break;
+__attribute__((used))
+__attribute__((interrupt("WCH-Interrupt-fast")))
+__attribute__((section(".highcode")))
+void TMR0_IRQHandler(void) {
+    if (TMR0_GetITFlag(TMR0_3_IT_CYC_END)) {
+        TMR0_ClearITFlag(TMR0_3_IT_CYC_END);
+        GPIOB_InverseBits(STATUS_LED_PIN);
     }
 }
---
+++ src/main.h
@@ -0,0 +1,10 @@
+#ifndef CH58KVM_MAIN_H
+#define CH58KVM_MAIN_H
+
+#include <stdint.h>
+
+
+#define STATUS_LED_PIN GPIO_Pin_18
+
+
+#endif //CH58KVM_MAIN_H
