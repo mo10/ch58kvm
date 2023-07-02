0a71b95ab679eb63625bdc1e3650e78d4ef75aec
mo10 <0w0@0w0.pw>
2023-07-03 00:13:18 +0800

    Support keyboard and mouse

--- src/usb/hid.c
+++ src/usb/hid.c
@@ -1,95 +1,186 @@
 #include "usbd_core.h"
-#include "usbd_hid.h"
 #include "hid.h"
 
 // pid.codes Test PID
 #define USBD_VID           0x1209
 #define USBD_PID           0001
-
 #define USBD_MAX_POWER     100
-#define USBD_LANGID_STRING 1033
+#define USBD_LANGID_STRING 0x0409
 
 // HID Keyboard endpoint in
 #define HID_KBD_EPIN USB_ENDPOINT_IN(1)
 #define HID_KBD_EPIN_SIZE 8
-#define HID_KBD_EPIN_INTERVAL 1
-// HID Keyboard endpoint out
+#define HID_KBD_EPIN_INTERVAL 0x01
+// HID Keyboard endpoint out (keyboard led)
 #define HID_KBD_EPOUT USB_ENDPOINT_OUT(1)
-#define HID_KBD_EPOUT_SIZE 8
-#define HID_KBD_EPOUT_INTERVAL 1
+#define HID_KBD_EPOUT_SIZE 1
+#define HID_KBD_EPOUT_INTERVAL 0x01
+// HID mouse endpoint in
+#define HID_MOUSE_EPIN USB_ENDPOINT_IN(2)
+#define HID_MOUSE_EPIN_SIZE 3
+#define HID_MOUSE_EPIN_INTERVAL 0x01
 
-#define HID_KBD_REPORT_DESC_SIZE 63
-
-#define USB_HID_CONFIG_DESC_SIZ (USB_SIZEOF_CONFIG_DESC     \
+#define USB_HID_CONFIG_DESC_SIZ ( USB_SIZEOF_CONFIG_DESC    \
+                                /* Keyboard interface  */   \
                                 + USB_SIZEOF_INTERFACE_DESC \
-                                + 9 /* HID_DESC */          \
+                                + USB_SIZEOF_HID_DESC       \
                                 + USB_SIZEOF_ENDPOINT_DESC  \
-                                + USB_SIZEOF_ENDPOINT_DESC)
+                                + USB_SIZEOF_ENDPOINT_DESC  \
+                                /* Mouse interface  */      \
+                                + USB_SIZEOF_INTERFACE_DESC \
+                                + USB_SIZEOF_HID_DESC       \
+                                + USB_SIZEOF_ENDPOINT_DESC  \
+                              )
 
-#define HID_KEYBOARD_REPORT_DESC_SIZE 63
+
+// https://www.usb.org/sites/default/files/hid1_11.pdf
+// Appendix E: Example USB Descriptors for HID Class Devices
+//   E.6 Report Descriptor (Keyboard)
+static const uint8_t hid_kbd_report_desc[] = {
+        0x05, 0x01,         // Usage Page (Generic Desktop)
+        0x09, 0x06,         // Usage (Keyboard)
+        0xa1, 0x01,         // Collection (Application)
+        0x05, 0x07,         //   Usage Page (Key Codes)
+        0x19, 0xe0,         //   Usage Minimum (224)
+        0x29, 0xe7,         //   Usage Maximum (231)
+        0x15, 0x00,         //   Logical Minimum (0)
+        0x25, 0x01,         //   Logical Maximum (1)
+        0x75, 0x01,         //   Report Size (1)
+        0x95, 0x08,         //   Report Count (8)
+        0x81, 0x02,         //   Input (Data, Variable, Absolute) ;Modifier byte
+        0x95, 0x01,         //   Report Count (1)
+        0x75, 0x08,         //   Report Size (8)
+        0x81, 0x01,         //   Input (Constant) ;Reserved byte
+        0x95, 0x05,         //   Report Count (5)
+        0x75, 0x01,         //   Report Size (1)
+        0x05, 0x08,         //   Usage Page (LEDs)
+        0x19, 0x01,         //   Usage Minimum (1)
+        0x29, 0x05,         //   Usage Maximum (5)
+        0x91, 0x02,         //   Output (Data, Variable, Absolute) ;LED report
+        0x95, 0x01,         //   Report Count (1)
+        0x75, 0x03,         //   Report Size (3)
+        0x91, 0x01,         //   Output (Constant) ;LED report padding
+        0x95, 0x06,         //   Report Count (6)
+        0x75, 0x08,         //   Report Size (8)
+        0x15, 0x00,         //   Logical Minimum (0)
+        0x25, 0x65,         //   Logical Maximum (101)
+        0x05, 0x07,         //   Usage Page (Key Codes)
+        0x19, 0x00,         //   Usage Minimum (0)
+        0x29, 0x65,         //   Usage Maximum (101)
+        0x81, 0x00,         //   Input (Data, Array) ;Key arrays (6 bytes)
+        0xc0                // End Collection
+};
+
+// Support 3 button + 12bits X 12bits Y + Wheel + Horizontal Wheel
+static const uint8_t hid_mouse_report_desc[] = {
+        0x05, 0x01,         // Usage Page (Generic Desktop)
+        0x09, 0x02,         // Usage (Mouse)
+        0xa1, 0x01,         // Collection (Application)
+        0x09, 0x01,         //   Usage (Pointer)
+        0xa1, 0x00,         //   Collection (Physical)
+        // 1byte: 3 Buttons
+        0x05, 0x09,         //     Usage Page (Buttons)
+        0x19, 0x01,         //     Usage Minimum (01)
+        0x29, 0x03,         //     Usage Maximum (03)
+        0x15, 0x00,         //     Logical Minimum (0)
+        0x25, 0x01,         //     Logical Maximum (1)
+        0x95, 0x03,         //     Report Count (3)
+        0x75, 0x01,         //     Report Size (1)
+        0x81, 0x02,         //     Input (Data, Variable, Absolute) ;3 button bits
+        0x95, 0x01,         //     Report Count (1)
+        0x75, 0x05,         //     Report Size (5)
+        0x81, 0x01,         //     Input (Constant) ;5 bit padding
+        // 12bits X + 12bits Y
+        0x05, 0x01,         //     Usage Page (Generic Desktop)
+        0x16, 0x01, 0xf8,   //     Logical Minimum (-2047)
+        0x26, 0xff, 0x07,   //     Logical Maximum (2047)
+        0x75, 0x0c,         //     Report Size (12)
+        0x95, 0x02,         //     Report Count (2)
+        0x09, 0x30,         //     Usage (X)
+        0x09, 0x31,         //     Usage (Y)
+        0x81, 0x06,         //     Input (Data, Variable, Relative) ;3 position bytes (12bit X & 12bit Y)
+        // 1byte Vertical Wheel
+        0x15, 0x81,         //     Logical Minimum (-127)
+        0x25, 0x7f,         //     Logical Maximum (127)
+        0x75, 0x08,         //     Report Size (8)
+        0x95, 0x01,         //     Report Count (1)
+        0x09, 0x38,         //     Usage (Wheel)
+        0x81, 0x06,         //     Input (Data, Variable, Relative) ;1 position byte Wheel
+        // 1byte Horizontal Wheel
+        0x05, 0x0c,         //     Usage Page (Consumer)
+        0x0a, 0x38, 0x02,    //     Usage (AC Pan)
+        0x95, 0x01,         //     Report Count (1)
+        0x81, 0x06,         //     Input (Data, Variable, Relative) ;1 position byte AC Pan
+        0xc0,               //   End Collection
+        0xc0                // End Collection
+};
 
 static const uint8_t hid_descriptor[] = {
         USB_DEVICE_DESCRIPTOR_INIT(USB_2_0, 0x00, 0x00, 0x00, USBD_VID, USBD_PID, 0x0002, 0x01),
-        USB_CONFIG_DESCRIPTOR_INIT(USB_HID_CONFIG_DESC_SIZ, 0x01, 0x01, USB_CONFIG_BUS_POWERED, USBD_MAX_POWER),
-        USB_INTERFACE_DESCRIPTOR_INIT(0x00, 0x00, 0x02, 0x03, HID_SUBCLASS_BOOTIF, HID_PROTOCOL_KEYBOARD, 0x00),
-        /******************** Descriptor of HID keyboard ********************/
-        0x09,                          /* bLength: HID Descriptor size */
-        HID_DESCRIPTOR_TYPE_HID,       /* bDescriptorType: HID */
-        0x11,                          /* bcdHID: HID Class Spec release number */
-        0x01,
-        0x00,                          /* bCountryCode: Hardware target country */
-        0x01,                          /* bNumDescriptors: Number of HID class descriptors to follow */
-        0x22,                          /* bDescriptorType */
-        HID_KEYBOARD_REPORT_DESC_SIZE, /* wItemLength: Total length of Report descriptor */
-        0x00,
-        /******************** Descriptor of endpoint in ********************/
+        USB_CONFIG_DESCRIPTOR_INIT(USB_HID_CONFIG_DESC_SIZ, 0x02, 0x01,
+                                   USB_CONFIG_BUS_POWERED, USBD_MAX_POWER),
+        /* Keyboard interface */
+        USB_INTERFACE_DESCRIPTOR_INIT(0x00, 0x00, 0x02, 0x03, HID_SUBCLASS_BOOTIF,
+                                      HID_PROTOCOL_KEYBOARD, 0x00),
+        USB_HID_DESCRIPTOR_INIT(0x0111, 0x00, 0x01, 0x22, sizeof(hid_kbd_report_desc)),
         USB_ENDPOINT_DESCRIPTOR_INIT(HID_KBD_EPIN, USB_ENDPOINT_TYPE_INTERRUPT, HID_KBD_EPIN_SIZE,
                                      HID_KBD_EPIN_INTERVAL),
         USB_ENDPOINT_DESCRIPTOR_INIT(HID_KBD_EPOUT, USB_ENDPOINT_TYPE_INTERRUPT, HID_KBD_EPOUT_SIZE,
                                      HID_KBD_EPOUT_INTERVAL),
+        /* Mouse interface */
+        USB_INTERFACE_DESCRIPTOR_INIT(0x01, 0x00, 0x01, 0x03, HID_SUBCLASS_BOOTIF,
+                                      HID_PROTOCOL_MOUSE, 0x01),
+        USB_HID_DESCRIPTOR_INIT(0x0111, 0x00, 0x01, 0x22, sizeof(hid_mouse_report_desc)),
+        USB_ENDPOINT_DESCRIPTOR_INIT(HID_MOUSE_EPIN, USB_ENDPOINT_TYPE_INTERRUPT, HID_MOUSE_EPIN_SIZE,
+                                     HID_MOUSE_EPIN_INTERVAL),
+
         ///////////////////////////////////////
-        /// string0 descriptor
+        /// string0 language ID
         ///////////////////////////////////////
         USB_LANGID_INIT(USBD_LANGID_STRING),
         ///////////////////////////////////////
-        /// string1 descriptor
+        /// string1 manufacturer
         ///////////////////////////////////////
-        0x14,                       /* bLength */
+        0x1E,                       /* bLength */
         USB_DESCRIPTOR_TYPE_STRING, /* bDescriptorType */
-        'C', 0x00,                  /* wcChar0 */
-        'h', 0x00,                  /* wcChar1 */
-        'e', 0x00,                  /* wcChar2 */
-        'r', 0x00,                  /* wcChar3 */
-        'r', 0x00,                  /* wcChar4 */
-        'y', 0x00,                  /* wcChar5 */
-        'U', 0x00,                  /* wcChar6 */
-        'S', 0x00,                  /* wcChar7 */
-        'B', 0x00,                  /* wcChar8 */
+        'A', 0x00,                  /* wcChar0 */
+        'r', 0x00,                  /* wcChar1 */
+        'u', 0x00,                  /* wcChar2 */
+        'b', 0x00,                  /* wcChar3 */
+        'a', 0x00,                  /* wcChar4 */
+        ' ', 0x00,                  /* wcChar5 */
+        'N', 0x00,                  /* wcChar6 */
+        'e', 0x00,                  /* wcChar7 */
+        't', 0x00,                  /* wcChar8 */
+        'w', 0x00,                  /* wcChar9 */
+        'o', 0x00,                  /* wcChar10 */
+        'r', 0x00,                  /* wcChar11 */
+        'k', 0x00,                  /* wcChar12 */
+        's', 0x00,                  /* wcChar13 */
         ///////////////////////////////////////
-        /// string2 descriptor
+        /// string2 product
         ///////////////////////////////////////
-        0x26,                       /* bLength */
+        0x24,                       /* bLength */
         USB_DESCRIPTOR_TYPE_STRING, /* bDescriptorType */
-        'C', 0x00,                  /* wcChar0 */
-        'h', 0x00,                  /* wcChar1 */
-        'e', 0x00,                  /* wcChar2 */
-        'r', 0x00,                  /* wcChar3 */
-        'r', 0x00,                  /* wcChar4 */
-        'y', 0x00,                  /* wcChar5 */
-        'U', 0x00,                  /* wcChar6 */
-        'S', 0x00,                  /* wcChar7 */
-        'B', 0x00,                  /* wcChar8 */
-        ' ', 0x00,                  /* wcChar9 */
-        'H', 0x00,                  /* wcChar10 */
-        'I', 0x00,                  /* wcChar11 */
-        'D', 0x00,                  /* wcChar12 */
-        ' ', 0x00,                  /* wcChar13 */
-        'D', 0x00,                  /* wcChar14 */
-        'E', 0x00,                  /* wcChar15 */
-        'M', 0x00,                  /* wcChar16 */
-        'O', 0x00,                  /* wcChar17 */
+        'R', 0x00,                  /* wcChar0 */
+        'e', 0x00,                  /* wcChar1 */
+        'm', 0x00,                  /* wcChar2 */
+        'o', 0x00,                  /* wcChar3 */
+        't', 0x00,                  /* wcChar4 */
+        'e', 0x00,                  /* wcChar5 */
+        ' ', 0x00,                  /* wcChar6 */
+        'U', 0x00,                  /* wcChar7 */
+        'S', 0x00,                  /* wcChar8 */
+        'B', 0x00,                  /* wcChar9 */
+        ' ', 0x00,                  /* wcChar10 */
+        'D', 0x00,                  /* wcChar11 */
+        'o', 0x00,                  /* wcChar12 */
+        'n', 0x00,                  /* wcChar13 */
+        'g', 0x00,                  /* wcChar14 */
+        'l', 0x00,                  /* wcChar15 */
+        'e', 0x00,                  /* wcChar16 */
         ///////////////////////////////////////
-        /// string3 descriptor
+        /// string3 serial
         ///////////////////////////////////////
         0x16,                       /* bLength */
         USB_DESCRIPTOR_TYPE_STRING, /* bDescriptorType */
@@ -121,42 +212,6 @@ static const uint8_t hid_descriptor[] = {
         0x00
 };
 
-static const uint8_t hid_keyboard_report_desc[HID_KEYBOARD_REPORT_DESC_SIZE] = {
-        0x05, 0x01, // USAGE_PAGE (Generic Desktop)
-        0x09, 0x06, // USAGE (Keyboard)
-        0xa1, 0x01, // COLLECTION (Application)
-        0x05, 0x07, // USAGE_PAGE (Keyboard)
-        0x19, 0xe0, // USAGE_MINIMUM (Keyboard LeftControl)
-        0x29, 0xe7, // USAGE_MAXIMUM (Keyboard Right GUI)
-        0x15, 0x00, // LOGICAL_MINIMUM (0)
-        0x25, 0x01, // LOGICAL_MAXIMUM (1)
-        0x75, 0x01, // REPORT_SIZE (1)
-        0x95, 0x08, // REPORT_COUNT (8)
-        0x81, 0x02, // INPUT (Data,Var,Abs)
-        0x95, 0x01, // REPORT_COUNT (1)
-        0x75, 0x08, // REPORT_SIZE (8)
-        0x81, 0x03, // INPUT (Cnst,Var,Abs)
-        0x95, 0x05, // REPORT_COUNT (5)
-        0x75, 0x01, // REPORT_SIZE (1)
-        0x05, 0x08, // USAGE_PAGE (LEDs)
-        0x19, 0x01, // USAGE_MINIMUM (Num Lock)
-        0x29, 0x05, // USAGE_MAXIMUM (Kana)
-        0x91, 0x02, // OUTPUT (Data,Var,Abs)
-        0x95, 0x01, // REPORT_COUNT (1)
-        0x75, 0x03, // REPORT_SIZE (3)
-        0x91, 0x03, // OUTPUT (Cnst,Var,Abs)
-        0x95, 0x06, // REPORT_COUNT (6)
-        0x75, 0x08, // REPORT_SIZE (8)
-        0x15, 0x00, // LOGICAL_MINIMUM (0)
-        0x25, 0xFF, // LOGICAL_MAXIMUM (255)
-        0x05, 0x07, // USAGE_PAGE (Keyboard)
-        0x19, 0x00, // USAGE_MINIMUM (Reserved (no event indicated))
-        0x29, 0x65, // USAGE_MAXIMUM (Keyboard Application)
-        0x81, 0x00, // INPUT (Data,Ary,Abs)
-        0xc0        // END_COLLECTION
-};
-
-
 USB_NOCACHE_RAM_SECTION USB_MEM_ALIGNX uint8_t read_buffer[CONFIG_USBDEV_MAX_BUS][64];
 USB_NOCACHE_RAM_SECTION USB_MEM_ALIGNX uint8_t write_buffer[CONFIG_USBDEV_MAX_BUS][64];
 
@@ -169,6 +224,7 @@ static volatile uint8_t hid_state[CONFIG_USBDEV_MAX_BUS] = {0};
 void usbd_event_handler(uint8_t busid, uint8_t event) {
     switch (event) {
         case USBD_EVENT_RESET:
+            USB_LOG_WRN("USBD_EVENT_RESET");
             hid_state[busid] = HID_STATE_IDLE;
             break;
         case USBD_EVENT_CONNECTED:
@@ -181,6 +237,7 @@ void usbd_event_handler(uint8_t busid, uint8_t event) {
             break;
         case USBD_EVENT_CONFIGURED:
             /* setup first out ep read transfer */
+
             usbd_ep_start_read(busid, HID_KBD_EPOUT, read_buffer[busid], 64);
             break;
         case USBD_EVENT_SET_REMOTE_WAKEUP:
@@ -193,28 +250,34 @@ void usbd_event_handler(uint8_t busid, uint8_t event) {
     }
 }
 
-static void usbd_hid_kbd_in_callback(uint8_t busid, uint8_t ep, uint32_t nbytes) {
-    USB_LOG_RAW("actual in len:%lu\r\n", nbytes);
+static void usbd_hid_in_callback(uint8_t busid, uint8_t ep, uint32_t nbytes) {
     hid_state[busid] = HID_STATE_IDLE;
 }
 
-static void usbd_hid_kbd_out_callback(uint8_t busid, uint8_t ep, uint32_t nbytes) {
-    USB_LOG_RAW("actual out len:%lu\r\n", nbytes);
+static volatile hid_kbd_led_report led_status[CONFIG_USBDEV_MAX_BUS] = {0};
+
+static void usbd_hid_kbd_led_callback(uint8_t busid, uint8_t ep, uint32_t nbytes) {
     usbd_ep_start_read(busid, HID_KBD_EPOUT, read_buffer[busid], 64);
+    led_status[busid] = *(hid_kbd_led_report *) read_buffer[busid];
 }
 
 static struct usbd_endpoint hid_kbd_epin = {
-        .ep_cb = usbd_hid_kbd_in_callback,
+        .ep_cb = usbd_hid_in_callback,
         .ep_addr = HID_KBD_EPIN
 };
 
 static struct usbd_endpoint hid_kbd_epout = {
-        .ep_cb = usbd_hid_kbd_out_callback,
+        .ep_cb = usbd_hid_kbd_led_callback,
         .ep_addr = HID_KBD_EPOUT
 };
 
-struct usbd_interface intf0[CONFIG_USBDEV_MAX_BUS];
+static struct usbd_endpoint hid_mouse_epin = {
+        .ep_cb = usbd_hid_in_callback,
+        .ep_addr = HID_MOUSE_EPIN
+};
 
+struct usbd_interface intf0[CONFIG_USBDEV_MAX_BUS];
+struct usbd_interface intf1[CONFIG_USBDEV_MAX_BUS];
 
 static struct usbd_bus ch58x_bus[CONFIG_USBDEV_MAX_BUS] = {
         {
@@ -231,25 +294,51 @@ extern struct usbd_udc_driver ch58xfs_udc_driver;
 void hid_init(uint8_t busid) {
     usbd_bus_add_udc(busid, ch58x_bus[busid].reg_base, &ch58xfs_udc_driver, NULL);
     usbd_desc_register(busid, hid_descriptor);
-    usbd_add_interface(busid,
-                       usbd_hid_init_intf(&intf0[busid],
-                                          hid_keyboard_report_desc,
-                                          HID_KEYBOARD_REPORT_DESC_SIZE));
+    usbd_add_interface(
+            busid,
+            usbd_hid_init_intf(
+                    &intf0[busid],
+                    hid_kbd_report_desc,
+                    sizeof(hid_kbd_report_desc))
+    );
+
+    usbd_add_interface(
+            busid,
+            usbd_hid_init_intf(
+                    &intf1[busid],
+                    hid_mouse_report_desc,
+                    sizeof(hid_mouse_report_desc))
+    );
+
     usbd_add_endpoint(busid, &hid_kbd_epin);
     usbd_add_endpoint(busid, &hid_kbd_epout);
+    usbd_add_endpoint(busid, &hid_mouse_epin);
 
     usbd_initialize(busid);
 }
 
-void hid_keyboard_test(uint8_t busid) {
-    const uint8_t sendbuffer[8] = {0x00, 0x00, HID_KBD_USAGE_A, 0x00, 0x00, 0x00, 0x00, 0x00};
+hid_kbd_led_report hid_get_keyboard_led_status(uint8_t busid) {
+    return led_status[busid];
+}
 
-    memcpy(write_buffer[busid], sendbuffer, 8);
-    int ret = usbd_ep_start_write(busid, HID_KBD_EPIN, write_buffer[busid], 8);
+void hid_send_keyboard_report(uint8_t busid, const hid_kbd_report *report) {
+    memcpy(write_buffer[busid], report, sizeof(hid_kbd_report));
+    int ret = usbd_ep_start_write(busid, HID_KBD_EPIN, write_buffer[busid], sizeof(hid_kbd_report));
     if (ret < 0) {
         return;
     }
+
     hid_state[busid] = HID_STATE_BUSY;
-    while (hid_state[busid] == HID_STATE_BUSY) {
+    while (hid_state[busid] == HID_STATE_BUSY);
+}
+
+void hid_send_mouse_report(uint8_t busid, const hid_mouse_report *report) {
+    memcpy(write_buffer[busid], report, sizeof(hid_mouse_report));
+    int ret = usbd_ep_start_write(busid, HID_MOUSE_EPIN, write_buffer[busid], sizeof(hid_mouse_report));
+    if (ret < 0) {
+        return;
     }
+
+    hid_state[busid] = HID_STATE_BUSY;
+    while (hid_state[busid] == HID_STATE_BUSY);
 }
--- src/usb/hid.h
+++ src/usb/hid.h
@@ -2,9 +2,63 @@
 #define HID_H
 
 #include <stdint.h>
+#include "usbd_hid.h"
 
+#define USB_SIZEOF_HID_DESC 0x09
+
+#define USB_HID_DESCRIPTOR_INIT(bcdHID, bCountryCode, bNumDescriptors, bDescriptorType, wItemLength) \
+    0x09,                          /* bLength */                                                     \
+    HID_DESCRIPTOR_TYPE_HID,       /* bDescriptorType */                                             \
+    WBVAL(bcdHID),                 /* bcdHID */                                                      \
+    bCountryCode,                  /* bCountryCode */                                                \
+    bNumDescriptors,               /* bNumDescriptors */                                             \
+    bDescriptorType,               /* bDescriptorType */                                             \
+    WBVAL(wItemLength)             /* wItemLength */                                                 \
+
+typedef struct __attribute__((packed)) {
+    struct __attribute__((packed)) {
+        uint8_t left_ctrl: 1;
+        uint8_t left_shift: 1;
+        uint8_t left_alt: 1;
+        uint8_t left_gui: 1;
+        uint8_t right_ctrl: 1;
+        uint8_t right_shift: 1;
+        uint8_t right_alt: 1;
+        uint8_t right_gui: 1;
+    } modifier;
+    uint8_t reserved;
+    uint8_t key[6];
+} hid_kbd_report;
+
+typedef struct __attribute__((packed)) {
+    uint8_t num_lock: 1;
+    uint8_t caps_lock: 1;
+    uint8_t scroll_lock: 1;
+    uint8_t compose: 1;
+    uint8_t kana: 1;
+    uint8_t reserved: 3;
+} hid_kbd_led_report;
+
+typedef struct __attribute__((packed)) {
+    struct __attribute__((packed)) {
+        uint8_t left: 1;
+        uint8_t right: 1;
+        uint8_t middle: 1;
+        uint8_t reserved: 5;
+    } buttons;
+    struct __attribute__((packed)) {
+        int16_t x: 12;
+        int16_t y: 12;
+    } pointer;
+    int8_t v_wheel;
+    int8_t h_wheel;
+} hid_mouse_report;
 
 void hid_init(uint8_t busid);
-void hid_keyboard_test(uint8_t busid);
+
+hid_kbd_led_report hid_get_keyboard_led_status(uint8_t busid);
+
+void hid_send_keyboard_report(uint8_t busid, const hid_kbd_report *report);
+void hid_send_mouse_report(uint8_t busid, const hid_mouse_report *report);
 
 #endif //HID_H
